4c90cfc9c1128f14d1dfe0a786e9ac614988a30e
IliqnIvanov <solommr5kk@gmail.com>
2016-03-31 22:31:37 +0300

    Added files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Age
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Convert.ToDateTime("12/02/21 10:56:09").ToString("MMM.dd,yyyy HH:mm:ss");
+
+
+            DateTime bDay = DateTime.Parse(Console.ReadLine());
+            DateTime today = DateTime.Now;
+            int age = today.Year - bDay.Year;
+
+            if (today < bDay.AddYears(age)) 
+            {
+                age--;
+            }
+            Console.WriteLine(age);
+            Console.WriteLine(age + 10);
+
+           
+        }
+    }
+}
